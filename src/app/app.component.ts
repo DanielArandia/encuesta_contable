006bf2abf05c1ac77e2a428ec2de71df420470bf
Daniel <daniel0803.felipe@gmail.com>
2024-11-03 22:24:32 -0500

    se modificaron los componentes css, html y ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule], // Añade CommonModule aquí
 })
 export class AppComponent {
-  title = 'encuesta_contable';
+  title = 'Asesoría Fiscal';
+  selectedForm: string | null = null; // Estado para rastrear qué formulario se muestra
+
+  showForm(formName: string) {
+    this.selectedForm = formName; // Actualiza el formulario seleccionado
+  }
+
+  // Función para manejar el envío de formularios (puedes personalizarlo más tarde)
+  onSubmit(event: Event) {
+    event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
+    alert('Formulario enviado!'); // Aquí puedes implementar la lógica para manejar los archivos
+  }
 }
